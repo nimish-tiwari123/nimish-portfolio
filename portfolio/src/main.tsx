2e998b7b020bb1e896f964da4c21de19a65ab0c1
nimish-tiwari123 <nimisht.6555@gmail.com>
2024-11-08 13:46:38 +0530

    new commit

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -10,7 +10,7 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+     <App />
   </Provider>
   </StrictMode>
 )
